6ef293d415feddc4abd25bffa36afd18bcc26431
Deepak Kumar <162731007+light17ln@users.noreply.github.com>
2024-03-08 17:25:49 +0000

    LC-33,34

---
+++ Searching-1/02_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int firstposition(vector<int>& arr, int target){
+        int low=0,high=arr.size()-1;
+        int first=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] == target){
+                first = mid;
+                high=mid-1; //for checking the left side
+            }
+            else if(arr[mid] < target){
+                low=mid+1;
+            }
+            else  //arr[mid]>target
+                high=mid-1;
+        }
+        return first;
+    }
+
+    int lastposition(vector<int>& arr, int target){
+        int low=0,high=arr.size()-1;
+        int last=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] == target){
+                last = mid;
+                low=mid+1; //for checking the right side
+            }
+            else if(arr[mid] < target){
+                low=mid+1;
+            }
+            else  //arr[mid]>target
+                high=mid-1;
+        }
+        return last;
+    }
+
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int first = firstposition(arr,target);
+        if(first == -1) return {-1,-1};  //means element nhi hai
+        int last = lastposition(arr,target);
+        return {first, last};
+    }
+};
