5bf0b62ff82a803d1445c21ad4fa75760255161f
Deepak Kumar <162731007+light17ln@users.noreply.github.com>
2024-03-08 18:03:06 +0000

    LC33

--- Searching-1/01_Search_in_Rotated_Sorted_Array.cpp
+++ Searching-1/01_Search_in_Rotated_Sorted_Array.cpp
@@ -1,18 +1,31 @@
+METHOD:-1
+
+int LinearSearch(vector<int>& arr, int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+
+
+METHOD:-2
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
+    int search(vector<int>& arr, int target) {
+        int low=0,high=arr.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[low]<=nums[mid]){ //means left wala sorted hai
-                if(nums[low]<=target && target<=nums[mid])
+            if(arr[mid] == target) return mid;
+            if(arr[low]<=arr[mid]){ //means left wala sorted hai
+                if(arr[low]<=target && target<=arr[mid])
                     high = mid-1;   //ab left side jayega
                 else
                     low = mid+1;
             }
-            else{ // nums[mid] <= nums[high]  right wala sorted hai
-                if(nums[mid]<=target && target<=nums[high])
+            else{ // arr[mid] <= arr[high]  right wala sorted hai
+                if(arr[mid]<=target && target<=arr[high])
                     low = mid+1;  //ab right jayega
                 else
                     high = mid-1;
