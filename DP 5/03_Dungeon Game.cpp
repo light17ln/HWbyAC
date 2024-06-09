de7c8de771e791a50bd66e7fab2a2ab2ea2bb922
Deepak Kumar <162731007+light17ln@users.noreply.github.com>
2024-06-09 15:59:53 +0530

    lc174

---
+++ DP 5/03_Dungeon Game.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+//lc174
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        if (dungeon.empty() || dungeon[0].empty())
+            return 0; // invalid
+        //rows = m,  cols = n
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = min(0, dungeon[i][j]);
+                } 
+
+                else if (i == m - 1) {
+                    dp[i][j] = min(0, dungeon[i][j] + dp[i][j + 1]);
+                }
+
+                else if (j == n - 1) {
+                    dp[i][j] = min(0, dungeon[i][j] + dp[i + 1][j]);
+                } 
+
+                else {
+                    dp[i][j] = min(0, dungeon[i][j] + max(dp[i][j + 1], dp[i + 1][j]));
+                }
+            }
+        }
+
+        return abs(dp[0][0]) + 1;
+    }
+};
